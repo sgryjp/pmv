f725ed36b8446902d74e89ef5e1c57061c1455e3
Suguru YAMAMOTO <sgryjp@gmail.com>
2019-10-18 13:00:07 +0900

    Add utility script for coverage

---
+++ tools/coverage.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+SCRIPT=`readlink -f $0`
+SCRIPT_PATH=`dirname $SCRIPT`
+
+cd "$SCRIPT_PATH/.."
+
+cargo test --no-run
+if [ $? -ne 0 ]; then
+    echo "Unit test failed."
+    exit 1
+fi
+
+kcov target/cov `find target/debug -regex 'target/debug/pmv-[^\.]+$'`
+if [ $? -ne 0 ]; then
+    echo "Failed to generate HTML coverage report."
+    exit 1
+fi
+
+echo "Generated report should be written as: target/cov/index.html"

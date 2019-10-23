801e5337a616c31312643e6a8dd99caf1ee2b2e9
Suguru Yamamoto <sgryjp@gmail.com>
2019-10-23 17:25:09 +0900

    Start implementing main()

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,65 @@
-use std::path::{Path, PathBuf};
+use std::cmp;
+use std::env;
+use std::path::Path;
+use std::process::exit;
 
 use pmv::walk;
 
+/// Replaces variables in the given destination path string using the given
+/// substrings.
+fn replace(dest_ptn: &str, substrings: &[String]) -> String {
+    let dest = dest_ptn.as_bytes();
+    let mut replaced = String::new();
+    let mut i = 0;
+    while i < dest.len() {
+        if dest[i] == b'#' && i + 1 < dest.len() && b'1' <= dest[i + 1] && dest[i + 1] <= b'9' {
+            let index = (dest[i + 1] - b'1') as usize;
+            let replacement = &substrings[index]; //TODO: Index out of range
+            replaced.push_str(replacement);
+            i += 2;
+        } else {
+            replaced.push(dest[i] as char);
+            i += 1;
+        }
+    }
+    replaced
+}
+
 fn main() {
-    let args: Vec<_> = std::env::args().collect();
+    let args: Vec<_> = env::args().collect();
     let src_ptns = &args[1];
     let dest_ptn: &str = &args[2];
 
     match walk(Path::new("."), src_ptns) {
-        Err(err) => println!("Error: {:?}", err),
-        Ok(sources) => {
-            for m in sources {
-                let (entry, matches) = m;
-                //println!("# {:?} {:?}", entry, matches);
-                let dest_bytes = dest_ptn.as_bytes();
-                let mut dest = String::new();
-                let mut i = 0;
-                while i < dest_bytes.len() {
-                    if dest_bytes[i] == 0x5c // Backslash
-                        && i + 1 < dest_bytes.len()
-                        && 0x30 <= dest_bytes[i + 1] // 0
-                        && dest_bytes[i + 1] <= 0x39
-                    // 9
-                    {
-                        let index = (dest_bytes[i + 1] - 0x30 - 1) as usize;
-                        let replacement = &matches[index];
-                        dest.push_str(&replacement);
-                        i += 2;
-                    } else {
-                        dest.push_str(&dest_ptn[i..=i]);
-                        i += 1;
-                    }
+        Err(err) => {
+            eprintln!("Error: {:?}", err);
+            exit(2);
+        }
+        Ok(matches) => {
+            let destinations: Vec<_> = matches
+                .iter()
+                .map(|x| replace(dest_ptn, &x.1[..]))
+                .collect();
+            let sources: Vec<_> = matches.iter().map(|x| x.0.path()).collect();
+            assert_eq!(sources.len(), destinations.len());
+
+            let src_max_len = sources
+                .iter()
+                .map(|x| x.to_str().unwrap().len())
+                .fold(0, |acc, x| cmp::max(acc, x));
+
+            let mut line = String::new();
+            for (src, dest) in sources.iter().zip(destinations.iter()) {
+                let src = src.to_str().unwrap();
+
+                line.clear();
+                line.push_str(src);
+                for _ in src.len()..src_max_len {
+                    line.push(' ');
                 }
-                println!("{:?} --> {:?}", &entry, &PathBuf::from(dest));
+                line.push_str(" --> "); //TODO: Wrap line if it's too long
+                line.push_str(dest);
+                println!("{}", line);
                 //std::fs::rename(&entry.path(), &PathBuf::from(dest));
             }
         }

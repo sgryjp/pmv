b7f1704a950fe252a860406a4d90c738897e4cbc
Suguru Yamamoto <sgryjp@gmail.com>
2022-06-18 23:29:43 +0900

    refactor: Extract common test code fragments

--- tests/integration.rs
+++ tests/integration.rs
@@ -3,24 +3,36 @@ use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
 
-#[named]
-#[allow(dead_code)]
-//#[test]
-fn swap_filenames() {
+fn prepare(function_name: &str) -> PathBuf {
     assert!(PathBuf::from("./Cargo.toml").exists());
-    let temp_dir = PathBuf::from(format!("temp/system/{}", function_name!()));
+    let temp_dir = PathBuf::from(format!("temp/system/{}", function_name));
 
     // Prepare files and directories to testing
     if temp_dir.exists() {
         fs::remove_dir_all(&temp_dir).expect("Failed to remove temp_dir");
     }
     fs::create_dir_all(&temp_dir).expect("Failed to create temp_dir");
+
+    temp_dir
+}
+
+fn make_command() -> Command {
+    let cmd_path = PathBuf::from("./target/debug/pmv").canonicalize().unwrap();
+    Command::new(cmd_path)
+}
+
+#[named]
+#[allow(dead_code)]
+//#[test]
+fn swap_filenames() {
+    let temp_dir = prepare(function_name!());
+
+    // Prepare files and directories to testing
     fs::write(&temp_dir.join("AB"), "AB").unwrap();
     fs::write(&temp_dir.join("BA"), "BA").unwrap();
 
     // Execute pmv
-    let cmd_path = PathBuf::from("./target/debug/pmv").canonicalize().unwrap();
-    let mut command = Command::new(cmd_path);
+    let mut command = make_command();
     let output = command
         .current_dir(&temp_dir)
         .arg("-v")

894a5f07345cf651cb06296184ed76ced72eb103
Suguru Yamamoto <sgryjp@gmail.com>
2022-06-18 23:31:19 +0900

    test: Add integration test for --dry-run

--- tests/integration.rs
+++ tests/integration.rs
@@ -22,6 +22,52 @@ fn make_command() -> Command {
 }
 
 #[named]
+#[test]
+fn dry_run() {
+    let temp_dir = prepare(function_name!());
+
+    // Prepare files and directories to testing
+    fs::write(&temp_dir.join("AA"), "AA").unwrap();
+    fs::write(&temp_dir.join("AB"), "AB").unwrap();
+
+    // Execute pmv with --dry-run
+    let mut command = make_command();
+    let output = command
+        .current_dir(&temp_dir)
+        .arg("--dry-run")
+        .arg("??")
+        .arg("B#2")
+        .output()
+        .expect("Failed to launch pmv (debug build)");
+    assert!(output.status.success());
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    assert!(stdout.contains("AA --> BA"));
+    assert!(stdout.contains("AB --> BB"));
+
+    // Confirm nothing moved
+    let path_ba = temp_dir.join("BA");
+    let path_bb = temp_dir.join("BB");
+    assert!(!path_ba.exists());
+    assert!(!path_bb.exists());
+
+    // Then do the same without --dry-run
+    let mut command = make_command();
+    let output = command
+        .current_dir(&temp_dir)
+        .arg("??")
+        .arg("B#2")
+        .output()
+        .expect("Failed to launch pmv (debug build)");
+    assert!(output.status.success());
+
+    // Confirm files were moved
+    assert!(path_ba.exists());
+    assert!(path_bb.exists());
+    assert_eq!(fs::read_to_string(&path_ba).unwrap(), "AA");
+    assert_eq!(fs::read_to_string(&path_bb).unwrap(), "AB");
+}
+
+#[named]
 #[allow(dead_code)]
 //#[test]
 fn swap_filenames() {

sgryjp/pmv

commit 7aca024ee2d526a8690399bea4f9947a9d09212b

feat: Sort moving actions in safe order

src/lib.rs
@@ -11,6 +11,7 @@ mod fsutil;
 mod plan;
 mod walk;
 use fsutil::move_files;
+use plan::sort_entries;
 use plan::substitute_variables;
 use walk::walk;
 
@@ -24,6 +25,7 @@ struct Config {
 }
 
 /// A pair of source and destination in a moving plan.
+#[derive(Clone, Debug, PartialEq)]
 pub struct Entry {
     src: PathBuf,
     dest: PathBuf,
@@ -185,6 +187,8 @@ pub fn try_main(args: &[OsString]) -> Result<(), String> {
 
     // Collect paths of the files to move with their destination
     let entries = matches_to_entries(config.src_ptn.as_str(), config.dest_ptn.as_str());
+
+    let entries = sort_entries(&entries)?;
 
     // Validate them
     validate(&entries)?;

src/plan.rs
@@ -1,4 +1,144 @@
-use std::path::MAIN_SEPARATOR;
+use crate::Entry;
+use rand::random;
+use std::path::{Path, PathBuf, MAIN_SEPARATOR};
+
+pub fn sort_entries(entries: &[Entry]) -> Result<Vec<Entry>, String> {
+    let mut entries: Vec<&Entry> = entries.iter().collect();
+    let mut sorted: Vec<Entry> = Vec::new();
+    while !entries.is_empty() {
+        // Pull a chain starting with the first entry.
+        let mut indices = pull_a_chain(&entries)?;
+        debug_assert!(0 < indices.len());
+
+        // Resolve if the end of the chain is the beginnig
+        let is_circular = if 2 <= indices.len() {
+            let first = entries[indices[0]];
+            let last = entries[*indices.last().unwrap()];
+            first.src == last.dest
+        } else {
+            false
+        };
+
+        // Copy the pulled entries in reverse order so that they can be executed safely.
+        if is_circular {
+            // If the moving plan makes a circle, insert a temporary node to break it
+            // (for example, turn [C→A, B→C, A→B] into [C→X, B→C, A→B, X→A].)
+            // To do that, firstly we resolve a temporary backup file name.
+            let first = entries[indices[0]];
+            let last = entries[*indices.last().unwrap()];
+            let tmp = match make_safeish_filename(&first.src) {
+                Some(path) => path,
+                None => {
+                    return Err(format!(
+                        "temporary filename unavailable for {}",
+                        first.src.to_string_lossy()
+                    ))
+                }
+            };
+            sorted.push(Entry {
+                src: last.src.clone(),
+                dest: tmp.clone(),
+            });
+            for i in indices.iter().rev().skip(1) {
+                sorted.push(entries[*i].clone());
+            }
+            sorted.push(Entry {
+                src: tmp, // move
+                dest: first.src.clone(),
+            });
+        } else {
+            for i in indices.iter().rev() {
+                sorted.push(entries[*i].clone());
+            }
+        }
+
+        // Now remove the pulled and copied entries from the source.
+        indices.sort();
+        for i in indices.iter().rev() {
+            entries.remove(*i);
+        }
+    }
+
+    Ok(sorted)
+}
+
+fn make_safeish_filename<P: AsRef<Path>>(path: P) -> Option<PathBuf> {
+    //TODO: Make this efficient
+    let path = path.as_ref();
+    if let Some(filename) = path.file_name() {
+        let n: u16 = random();
+        for i in (n..65535).chain(0..n) {
+            let mut new_filename = filename.to_owned();
+            new_filename.push(format!(".pmv{:04x}", i));
+            let new_path = path.with_file_name(new_filename);
+            if new_path.exists() {
+                continue;
+            }
+            return Some(new_path);
+        }
+    }
+    None
+}
+
+/// Enumerates a chain of moving actions which must be done in reversed order.
+///
+/// This function does not detect circular network so the caller must take care of that case.
+/// If two or more entries shares a src, this function fails.
+fn pull_a_chain(entries: &[&Entry]) -> Result<Vec<usize>, String> {
+    let mut indices: Vec<usize> = vec![];
+
+    // If there is nothing to move, we are done.
+    if entries.len() == 0 {
+        return Ok(indices);
+    }
+
+    // Remember the first entry for later
+    let _head = &entries[0];
+    if let Some(e) = entries.iter().skip(1).find(|e| e.src == _head.src) {
+        // Fail if there is another entry of which src is the same
+        return Err(format!(
+            "cannot move a file to mutliple destinations: '{}' to '{}' and '{}'",
+            _head.src.to_string_lossy(),
+            _head.dest.to_string_lossy(),
+            e.dest.to_string_lossy()
+        ));
+    }
+    indices.push(0);
+
+    loop {
+        let prev_indices_len = indices.len();
+
+        // Find an entry which can be chained. (e.g.: B→C after A→B)
+        for (i, entry) in entries.iter().enumerate().skip(1) {
+            // Skip if this entry cannot be as such.
+            let curr = entries[*indices.last().unwrap()];
+            if entry.src != curr.dest {
+                continue;
+            }
+
+            // Fail if the src was shared with other entries.
+            if let Some(e) = entries.iter().skip(i + 1).find(|e| e.src == curr.dest) {
+                return Err(format!(
+                    "cannot move a file to mutliple destinations: '{}' to '{}' and '{}'",
+                    entry.src.to_string_lossy(),
+                    entry.dest.to_string_lossy(),
+                    e.dest.to_string_lossy(),
+                ));
+            }
+
+            // Remember this as a following entry.
+            indices.push(i);
+            break;
+        }
+
+        // Exit if no chaining entry was found.
+        if indices.len() == prev_indices_len {
+            break;
+        }
+    }
+
+    Ok(indices)
+}
 
 /// Substitute variables with substrings.
 ///
@@ -246,6 +386,138 @@ mod tests {
             assert_eq!(
                 substitute_variables(dest, &substrs[..]),
                 format!("foo{}/{}/", SEP, SEP)
+            );
+        }
+    }
+
+    mod pull_a_chain {
+        use super::*;
+
+        #[test]
+        fn empty() {
+            let entries: Vec<&Entry> = vec![];
+            let indices = pull_a_chain(&entries);
+            assert!(indices.is_ok());
+            let indices = indices.unwrap();
+            assert_eq!(indices.len(), 0);
+        }
+
+        #[test]
+        fn single() {
+            let entries = vec![Entry::from_str("A", "B")];
+            let entries: Vec<&Entry> = entries.iter().collect();
+            let indices = pull_a_chain(&entries);
+            assert!(indices.is_ok());
+            let indices = indices.unwrap();
+            assert_eq!(indices.len(), 1);
+            assert_eq!(indices[0], 0);
+        }
+
+        #[test]
+        fn chained() {
+            let entries = vec![
+                Entry::from_str("A", "B"),
+                Entry::from_str("C", "X"),
+                Entry::from_str("B", "C"),
+            ];
+            let entries: Vec<&Entry> = entries.iter().collect();
+            let indices = pull_a_chain(&entries);
+            assert!(indices.is_ok());
+            let indices = indices.unwrap();
+            assert_eq!(indices, vec![0, 2, 1]);
+        }
+
+        #[test]
+        fn circular() {
+            let entries = vec![
+                Entry::from_str("A", "B"),
+                Entry::from_str("C", "A"),
+                Entry::from_str("B", "C"),
+            ];
+            let entries: Vec<&Entry> = entries.iter().collect();
+            let indices = pull_a_chain(&entries);
+            assert!(indices.is_ok());
+            let indices = indices.unwrap();
+            assert_eq!(indices, vec![0, 2, 1]);
+        }
+
+        #[test]
+        fn shared_src_1st() {
+            let entries = vec![Entry::from_str("A", "B"), Entry::from_str("A", "C")];
+            let entries: Vec<&Entry> = entries.iter().collect();
+            let indices = pull_a_chain(&entries);
+            assert!(indices.is_err());
+            let msg = indices.unwrap_err();
+            assert!(msg.contains("'A' to 'B' and 'C'"));
+        }
+
+        #[test]
+        fn shared_src_2nd() {
+            let entries = vec![
+                Entry::from_str("A", "B"),
+                Entry::from_str("B", "C"),
+                Entry::from_str("B", "D"),
+            ];
+            let entries: Vec<&Entry> = entries.iter().collect();
+            let indices = pull_a_chain(&entries);
+            assert!(indices.is_err());
+            let msg = indices.unwrap_err();
+            assert!(msg.contains("'B' to 'C' and 'D'"));
+        }
+    }
+
+    mod sort_entries {
+        use super::*;
+
+        #[test]
+        fn emtpy() {
+            let entries: Vec<Entry> = vec![];
+            let sorted = sort_entries(&entries).unwrap();
+            assert_eq!(sorted.len(), 0);
+        }
+
+        #[test]
+        fn single() {
+            let entries = vec![Entry::from_str("A", "B")];
+            let sorted = sort_entries(&entries).unwrap();
+            assert_eq!(sorted, vec![Entry::from_str("A", "B")]);
+        }
+
+        #[test]
+        fn chained() {
+            let entries = vec![
+                Entry::from_str("A", "B"),
+                Entry::from_str("C", "X"),
+                Entry::from_str("B", "C"),
+            ];
+            let sorted = sort_entries(&entries).unwrap();
+            assert_eq!(
+                sorted,
+                vec![
+                    Entry::from_str("C", "X"),
+                    Entry::from_str("B", "C"),
+                    Entry::from_str("A", "B"),
+                ]
+            );
+        }
+
+        #[test]
+        fn circular() {
+            let entries = vec![
+                Entry::from_str("A", "B"),
+                Entry::from_str("C", "A"),
+                Entry::from_str("B", "C"),
+            ];
+            let sorted = sort_entries(&entries).unwrap();
+            let tmp = sorted[0].dest.to_str().unwrap();
+            assert_eq!(
+                sorted,
+                vec![
+                    Entry::from_str("C", tmp),
+                    Entry::from_str("B", "C"),
+                    Entry::from_str("A", "B"),
+                    Entry::from_str(tmp, "A"),
+                ]
             );
         }
     }

Cargo.toml
@@ -12,6 +12,7 @@ edition = "2018"
 [dependencies]
 ansi_term = "~0.12.1"
 atty = "~0.2"
+rand = "0.8.5"
 
 [dependencies.clap]
 version = "3.2.5"

commit 706bcdf22bd4936f5d49015218190419a362e679

fix: Use absolute path internally
Because previously the path strings collected were relative, pmv wrongly
recognize `./foo` and `foo` were different. This problem have corrupted
the sorting logic introduced in 7aca024ee2d526a8690399bea4f9947a9d09212b.

src/lib.rs
@@ -130,7 +130,9 @@ fn parse_args(args: &[OsString]) -> Config {
 }
 
 fn matches_to_entries(src_ptn: &str, dest_ptn: &str) -> Vec<Entry> {
-    let matches = match walk(Path::new("."), src_ptn) {
+    //TODO: Fix for when curdir is not available
+    let curdir = std::env::current_dir().unwrap();
+    let matches = match walk(curdir.as_path(), src_ptn) {
         Err(err) => {
             eprintln!(
                 "{}: failed to scan directory tree: {}",
@@ -146,7 +148,7 @@ fn matches_to_entries(src_ptn: &str, dest_ptn: &str) -> Vec<Entry> {
     for m in matches {
         let src = m.path();
         let dest = substitute_variables(dest_ptn, &m.matched_parts[..]);
-        let dest = PathBuf::from(dest);
+        let dest = curdir.join(dest);
         entries.push(Entry { src, dest });
     }
     entries

src/plan.rs
@@ -111,6 +111,9 @@ fn pull_a_chain(entries: &[&Entry]) -> Result<Vec<usize>, String> {
 
         // Find an entry which can be chained. (e.g.: B→C after A→B)
         for (i, entry) in entries.iter().enumerate().skip(1) {
+            debug_assert!(entry.src.is_absolute());
+            debug_assert!(entry.dest.is_absolute());
+
             // Skip if this entry cannot be as such.
             let curr = entries[*indices.last().unwrap()];
             if entry.src != curr.dest {
@@ -180,6 +183,18 @@ pub fn substitute_variables(dest: &str, substrings: &[String]) -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn to_absolute(entries: Vec<Entry>) -> Vec<Entry> {
+        let curdir = std::env::current_dir().unwrap();
+        entries
+            .iter()
+            .map(|entry| {
+                let src = curdir.join(&entry.src);
+                let dest = curdir.join(&entry.dest);
+                Entry { src, dest }
+            })
+            .collect()
+    }
 
     mod substitute_variables {
         use super::*;
@@ -405,7 +420,7 @@ mod tests {
 
         #[test]
         fn single() {
-            let entries = vec![Entry::from_str("A", "B")];
+            let entries = to_absolute(vec![Entry::from_str("A", "B")]);
             let entries: Vec<&Entry> = entries.iter().collect();
             let indices = pull_a_chain(&entries);
             assert!(indices.is_ok());
@@ -416,11 +431,11 @@ mod tests {
 
         #[test]
         fn chained() {
-            let entries = vec![
+            let entries = to_absolute(vec![
                 Entry::from_str("A", "B"),
                 Entry::from_str("C", "X"),
                 Entry::from_str("B", "C"),
-            ];
+            ]);
             let entries: Vec<&Entry> = entries.iter().collect();
             let indices = pull_a_chain(&entries);
             assert!(indices.is_ok());
@@ -430,11 +445,11 @@ mod tests {
 
         #[test]
         fn circular() {
-            let entries = vec![
+            let entries = to_absolute(vec![
                 Entry::from_str("A", "B"),
                 Entry::from_str("C", "A"),
                 Entry::from_str("B", "C"),
-            ];
+            ]);
             let entries: Vec<&Entry> = entries.iter().collect();
             let indices = pull_a_chain(&entries);
             assert!(indices.is_ok());
@@ -444,26 +459,32 @@ mod tests {
 
         #[test]
         fn shared_src_1st() {
-            let entries = vec![Entry::from_str("A", "B"), Entry::from_str("A", "C")];
+            let entries = to_absolute(vec![Entry::from_str("A", "B"), Entry::from_str("A", "C")]);
             let entries: Vec<&Entry> = entries.iter().collect();
             let indices = pull_a_chain(&entries);
             assert!(indices.is_err());
             let msg = indices.unwrap_err();
-            assert!(msg.contains("'A' to 'B' and 'C'"));
+            assert!(msg.contains("cannot move a file to mutliple destinations"));
+            assert!(msg.contains("A' to"));
+            assert!(msg.contains("B' and"));
+            assert!(msg.ends_with("C'"));
         }
 
         #[test]
         fn shared_src_2nd() {
-            let entries = vec![
+            let entries = to_absolute(vec![
                 Entry::from_str("A", "B"),
                 Entry::from_str("B", "C"),
                 Entry::from_str("B", "D"),
-            ];
+            ]);
             let entries: Vec<&Entry> = entries.iter().collect();
             let indices = pull_a_chain(&entries);
             assert!(indices.is_err());
             let msg = indices.unwrap_err();
-            assert!(msg.contains("'B' to 'C' and 'D'"));
+            assert!(msg.contains("cannot move a file to mutliple destinations"));
+            assert!(msg.contains("B' to"));
+            assert!(msg.contains("C' and"));
+            assert!(msg.ends_with("D'"));
         }
     }
 
@@ -479,46 +500,46 @@ mod tests {
 
         #[test]
         fn single() {
-            let entries = vec![Entry::from_str("A", "B")];
+            let entries = to_absolute(vec![Entry::from_str("A", "B")]);
             let sorted = sort_entries(&entries).unwrap();
-            assert_eq!(sorted, vec![Entry::from_str("A", "B")]);
+            assert_eq!(sorted, to_absolute(vec![Entry::from_str("A", "B")]));
         }
 
         #[test]
         fn chained() {
-            let entries = vec![
+            let entries = to_absolute(vec![
                 Entry::from_str("A", "B"),
                 Entry::from_str("C", "X"),
                 Entry::from_str("B", "C"),
-            ];
+            ]);
             let sorted = sort_entries(&entries).unwrap();
             assert_eq!(
                 sorted,
-                vec![
+                to_absolute(vec![
                     Entry::from_str("C", "X"),
                     Entry::from_str("B", "C"),
                     Entry::from_str("A", "B"),
-                ]
+                ])
             );
         }
 
         #[test]
         fn circular() {
-            let entries = vec![
+            let entries = to_absolute(vec![
                 Entry::from_str("A", "B"),
                 Entry::from_str("C", "A"),
                 Entry::from_str("B", "C"),
-            ];
+            ]);
             let sorted = sort_entries(&entries).unwrap();
             let tmp = sorted[0].dest.to_str().unwrap();
             assert_eq!(
                 sorted,
-                vec![
+                to_absolute(vec![
                     Entry::from_str("C", tmp),
                     Entry::from_str("B", "C"),
                     Entry::from_str("A", "B"),
                     Entry::from_str(tmp, "A"),
-                ]
+                ])
             );
         }
     }

src/walk.rs
@@ -27,7 +27,18 @@ impl Match {
 /// This function recursively search directory tree for entries matching the
 /// given pattern. While this function walks the directory tree, it remembers
 /// which part of the path corresponds to which wildcard in the pattern.
+///
+/// Note that this function expects the current directory is available.
+/// In that case, this function fails.
 pub fn walk(dir: &Path, pattern: &str) -> Result<Vec<Match>, String> {
+    //TODO: can we make `dir` AsRef<Path>?
+    if !dir.is_absolute() {
+        return Err(format!(
+            "needs an absolute directory path: {}",
+            dir.to_string_lossy()
+        ));
+    }
+
     let mut matches: Vec<Match> = Vec::new();
     let mut matched_parts: Vec<String> = Vec::new();
     let patterns: Vec<Component> = Path::new(pattern).components().collect();
@@ -149,8 +160,9 @@ mod tests {
         use super::*;
 
         fn setup(id: &str) {
-            let _ = fs::create_dir(Path::new("temp"));
-            let _ = fs::remove_dir_all(Path::new(&format!("temp/{}", id)));
+            let curdir = std::env::current_dir().unwrap();
+            let _ = fs::create_dir(curdir.join("temp"));
+            let _ = fs::remove_dir_all(curdir.join(&format!("temp/{}", id)));
             for dir1 in ["foo", "bar", "baz"].iter() {
                 for dir2 in ["foo", "bar", "baz"].iter() {
                     let _ =
@@ -165,7 +177,9 @@ mod tests {
 
         fn new_setup(id: &str, prereq_dirs: Vec<&str>, prereq_files: Vec<&str>) -> PathBuf {
             // Prepare working directory
-            let workdir = Path::join(Path::new("temp"), id);
+            let mut workdir = std::env::current_dir().unwrap();
+            workdir.push("temp");
+            workdir.push(id);
             let _ = fs::remove_dir_all(workdir.as_path());
             fs::create_dir_all(workdir.as_path()).unwrap();
 
@@ -180,13 +194,25 @@ mod tests {
             return workdir;
         }
 
+        #[test]
+        fn non_absolute_search_root() {
+            let result = walk(&PathBuf::from("."), "*");
+            assert!(result.is_err());
+            let err = result.err().unwrap();
+            assert!(err.contains("needs an absolute directory path"));
+        }
+
         #[named]
         #[test]
         fn no_specials() {
             setup(function_name!());
-            let matches = walk(Path::new("temp/no_specials"), "foo/bar/baz").unwrap();
+            let curdir = std::env::current_dir().unwrap();
+            let matches = walk(&curdir.join("temp/no_specials"), "foo/bar/baz").unwrap();
             assert_eq!(matches.len(), 1);
-            assert_eq!(matches[0].path(), Path::new("temp/no_specials/foo/bar/baz"));
+            assert_eq!(
+                matches[0].path(),
+                curdir.join("temp/no_specials/foo/bar/baz")
+            );
             assert_eq!(matches[0].matched_parts, Vec::<String>::new());
         }
 
@@ -194,7 +220,8 @@ mod tests {
         #[test]
         fn question() {
             setup(function_name!());
-            let mut matches = walk(Path::new("temp/question"), "ba?/ba?/ba?").unwrap();
+            let curdir = std::env::current_dir().unwrap();
+            let mut matches = walk(&curdir.join("temp/question"), "ba?/ba?/ba?").unwrap();
             assert_eq!(matches.len(), 8);
             matches.sort_by(|a, b| a.path().cmp(&b.path()));
 
@@ -202,14 +229,14 @@ mod tests {
             assert_eq!(
                 paths,
                 vec![
-                    Path::new("temp/question/bar/bar/bar"),
-                    Path::new("temp/question/bar/bar/baz"),
-                    Path::new("temp/question/bar/baz/bar"),
-                    Path::new("temp/question/bar/baz/baz"),
-                    Path::new("temp/question/baz/bar/bar"),
-                    Path::new("temp/question/baz/bar/baz"),
-                    Path::new("temp/question/baz/baz/bar"),
-                    Path::new("temp/question/baz/baz/baz"),
+                    curdir.join("temp/question/bar/bar/bar"),
+                    curdir.join("temp/question/bar/bar/baz"),
+                    curdir.join("temp/question/bar/baz/bar"),
+                    curdir.join("temp/question/bar/baz/baz"),
+                    curdir.join("temp/question/baz/bar/bar"),
+                    curdir.join("temp/question/baz/bar/baz"),
+                    curdir.join("temp/question/baz/baz/bar"),
+                    curdir.join("temp/question/baz/baz/baz"),
                 ]
             );
 
@@ -240,7 +267,8 @@ mod tests {
         #[test]
         fn star() {
             setup(function_name!());
-            let mut matches = walk(Path::new("temp/star"), "b*/b*/b*").unwrap();
+            let curdir = std::env::current_dir().unwrap();
+            let mut matches = walk(&curdir.join("temp/star"), "b*/b*/b*").unwrap();
             assert_eq!(matches.len(), 8);
             matches.sort_by(|a, b| a.path().cmp(&b.path()));
 
@@ -248,14 +276,14 @@ mod tests {
             assert_eq!(
                 paths,
                 vec![
-                    Path::new("temp/star/bar/bar/bar"),
-                    Path::new("temp/star/bar/bar/baz"),
-                    Path::new("temp/star/bar/baz/bar"),
-                    Path::new("temp/star/bar/baz/baz"),
-                    Path::new("temp/star/baz/bar/bar"),
-                    Path::new("temp/star/baz/bar/baz"),
-                    Path::new("temp/star/baz/baz/bar"),
-                    Path::new("temp/star/baz/baz/baz"),
+                    curdir.join("temp/star/bar/bar/bar"),
+                    curdir.join("temp/star/bar/bar/baz"),
+                    curdir.join("temp/star/bar/baz/bar"),
+                    curdir.join("temp/star/bar/baz/baz"),
+                    curdir.join("temp/star/baz/bar/bar"),
+                    curdir.join("temp/star/baz/bar/baz"),
+                    curdir.join("temp/star/baz/baz/bar"),
+                    curdir.join("temp/star/baz/baz/baz"),
                 ]
             );
 
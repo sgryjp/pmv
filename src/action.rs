33f0dc8d28649c202b6ed5406dfd156a442128c9
Suguru Yamamoto <sgryjp@gmail.com>
2022-07-03 00:00:43 +0900

    refactor: Add test for #[derive(Debug)]
    ...to raise test coverage.

--- src/action.rs
+++ src/action.rs
@@ -8,6 +8,7 @@ pub struct Action {
 }
 
 impl Action {
+    /// Creates an Action.
     pub fn new<P1: Into<PathBuf>, P2: Into<PathBuf>>(src: P1, dest: P2) -> Action {
         Action {
             src: src.into(),
@@ -15,10 +16,12 @@ impl Action {
         }
     }
 
+    /// Returns the path to the file to move.
     pub fn src(self: &Action) -> &Path {
         self.src.as_path()
     }
 
+    /// Returns the location to where the targeting file is moved.
     pub fn dest(self: &Action) -> &Path {
         self.dest.as_path()
     }
@@ -27,5 +30,19 @@ impl Action {
 impl<'a> From<&'a Action> for (&'a Path, &'a Path) {
     fn from(action: &'a Action) -> (&'a Path, &'a Path) {
         (action.src.as_path(), action.dest.as_path())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn derive_debug() {
+        let action = Action::new("A", "B");
+        assert_eq!(
+            format!("{:?}", action),
+            "Action { src: \"A\", dest: \"B\" }"
+        );
     }
 }
